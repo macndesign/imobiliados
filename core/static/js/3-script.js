4a4c7dcb463750145e4833a4d8024d1f4ddff65e
macndesign <macndesign@gmail.com>
2014-06-13 13:34:05 +0000

    script para alinhar destaques ou a altura de qalquer conjunto de elementos

--- core/static/js/3-script.js
+++ core/static/js/3-script.js
@@ -2,35 +2,49 @@
  * Created by mario on 15/05/14.
  */
 // Can also be used with $(document).ready()
-$(window).load(function () {
-    $('.flexslider-principal').flexslider({
-        animation: "slide",
-        slideshowSpeed: 5000,
-        startAt: 0,
-        slideshow: true
-    });
-    $('.flexslider-destaque').flexslider({
-        animation: "slide",
-        controlNav: "thumbnails",
-        randomize: false,
-        slideshow: false
-    });
 
-    // Detalhe
-    $('#flexslider-detalhe-thumb').flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: false,
-        slideshow: false,
-        itemWidth: 120,
-        itemMargin: 5,
-        asNavFor: '#flexslider-detalhe'
+var get_greater_height = function(objs) {
+    var greater = 0;
+    objs.each(function(i){
+        var h = objs.eq(i).height();
+        if (h > greater) { greater = h }
     });
-    $('#flexslider-detalhe').flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: false,
-        slideshow: false,
-        sync: "#flexslider-detalhe-thumb"
+    return greater;
+};
+var set_greater_height = function(objs, greater, el_height) {
+    objs.each(function(i){
+        this.style.height = (el_height + greater) + 'px';
     });
+};
+
+
+$('.flexslider-principal').flexslider({
+    animation: "slide",
+    slideshowSpeed: 5000,
+    startAt: 0,
+    slideshow: true
+});
+$('.flexslider-destaque').flexslider({
+    animation: "slide",
+    controlNav: "thumbnails",
+    randomize: false,
+    slideshow: false
+});
+
+// Detalhe
+$('#flexslider-detalhe-thumb').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 120,
+    itemMargin: 5,
+    asNavFor: '#flexslider-detalhe'
+});
+$('#flexslider-detalhe').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#flexslider-detalhe-thumb"
 });
